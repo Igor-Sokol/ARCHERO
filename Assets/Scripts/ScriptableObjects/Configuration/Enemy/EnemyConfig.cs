ce681ae30fa672b2d1cb71cd4608325d1432144c
Igor Sokol <igor.sokol.isa@gmail.com>
2022-11-14 16:51:27 +0300

    Add levelGenerator

---
+++ Assets/Scripts/ScriptableObjects/Configuration/Enemy/EnemyConfig.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Enemy", menuName = "LevelObjectConfiguration/EnemyConfig", order = 1)]
+public class EnemyConfig : LevelObjectConfiguration
+{
+    public int damage;
+}
